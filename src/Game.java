955dfdd98f67e2a6dbdefaa3c8e6eeb94add2fa6
kgacek <krzysiek.gacus@wp.pl>
2015-04-14 19:45:32 +0200

    racquets motion added

--- src/Game.java
+++ src/Game.java
@@ -1,5 +1,7 @@
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 import javax.swing.JPanel;
 
@@ -7,6 +9,26 @@ import javax.swing.JPanel;
 public class Game extends JPanel {
 	Ball ball=new Ball(this);
 	Racquets rac=new Racquets(this);
+	
+	
+	public Game(){
+		addKeyListener(new KeyListener() {
+			@Override
+			public void keyTyped(KeyEvent e) {
+			}
+
+			@Override
+			public void keyReleased(KeyEvent e) {
+				rac.keyReleased(e);
+			}
+
+			@Override
+			public void keyPressed(KeyEvent e) {
+				rac.keyPressed(e);
+			}
+		});
+		setFocusable(true);
+	}
 	
 	public void paint(Graphics g){
 	super.paint(g);
